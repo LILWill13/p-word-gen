3d09192bb1af3548d848b373cfb5f07687026bb9
noah gaston <noahsfuture18@gmail.com>
2022-03-23 00:59:56 -0400

    working pop up message

--- script copy.js
+++ script copy.js
@@ -1,14 +1,62 @@
 // Assignment Code
+// generateBtn is linked to the button
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn)
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const openPopButtons = document.querySelectorAll('[data-pop-target]')
+const clsPopButtons = document.querySelectorAll('[data-cls-btn]')
+const overlay = document.getElementById('overlay')
 
-  passwordText.value = password;
+openPopButtons.forEach(button => {
+  button.addEventListener('click',() => {
+    const criteria = document.querySelector(button.dataset.popTarget)
+    openPop(criteria)
+  })
+})
 
+overlay.addEventListener('click', () => {
+  const pop = document.querySelectorAll('#criteria.active')
+  pop.forEach(criteria => {
+    clsPop(criteria)
+  })
+})
+
+clsPopButtons.forEach(button => {
+  button.addEventListener('click',() => {
+    const criteria = button.closest('#criteria')
+    clsPop(criteria)
+  })
+})
+
+function openPop(criteria) {
+  if(criteria === null) return
+  criteria.classList.add('active')
+  overlay.classList.add('active')
 }
 
+function clsPop(criteria) {
+  if(criteria === null) return
+  criteria.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+// Write password to the #password input
+// function writePassword() {
+
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", function showPassword(){
+//   console.log(writePassword())
+// });
+
+
+
+
+
+// console.log()
