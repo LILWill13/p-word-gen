050839fd3a0039bb02bba4c9f66e4810b8bbb8c7
noah gaston <noahsfuture18@gmail.com>
2022-03-23 23:29:36 -0400

    mostly functunal js

--- script copy.js
+++ script copy.js
@@ -1,11 +1,23 @@
 // Assignment Code
-// generateBtn is linked to the button
-var generateBtn = document.querySelector("#generate");
-console.log(generateBtn)
-
 const openPopButtons = document.querySelectorAll('[data-pop-target]')
 const clsPopButtons = document.querySelectorAll('[data-cls-btn]')
 const overlay = document.getElementById('overlay')
+
+const randomCharacter = "abcdefghijklmnopqrstuvwxyz"
+const randomSpecial = "!#$%&*-_+()^~@"
+
+const alpha = randomCharacter[Math.floor(Math.random() * randomCharacter.length)];
+const alphaUp = randomCharacter[Math.floor(Math.random() * randomCharacter.length)].toUpperCase();
+const ranChar = randomSpecial[Math.floor(Math.random() * randomSpecial.length)];
+const number = Math.floor(Math.random() * 10);
+
+
+const funcs = {
+  alpha: alpha,
+  alphaUp: alphaUp,
+  ranChar: ranChar,
+  number: number,
+}
 
 openPopButtons.forEach(button => {
   button.addEventListener('click',() => {
@@ -40,23 +52,69 @@ function clsPop(criteria) {
   overlay.classList.remove('active')
 }
 
-// Write password to the #password input
-// function writePassword() {
 
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+// generateBtn is linked to the submit button
+var generateBtn = document.querySelector("#submit");
 
-//   passwordText.value = password;
+const form = document.getElementById('buttons');
+const length = document.getElementById('length'); 
+const lowercase = document.getElementById("lowercase"); 
+const uppercase = document.getElementById('uppercase'); 
+const numeric = document.getElementById('numeric'); 
+const special = document.getElementById('special-char');  
 
-// }
 
+
+form.addEventListener('submit', () => {
+    const lengthValue = +length.value
+    const lowerCheck = lowercase.checked
+    const upperCheck = uppercase.checked
+    const numericCheck = numeric.checked
+    const speicalChecked = special.checked
+
+   generatePassword(lowerCheck, upperCheck, numericCheck, speicalChecked, lengthValue);
+});
+
+function generatePassword(alpha, alphaUp, number, ranChar, length) {
+
+    let generatedPassword ="";
+
+    const allCriteria = alpha + alphaUp + number + ranChar;
+
+    const  criteriaArr = [{alpha}, {alphaUp}, {number}, {ranChar}].filter(item =>  Object.values(item)[0]);
+
+    if (allCriteria === 0) {
+       alert('Set requirements');
+    } else if (length < 8 || length > 128) {
+      alert('Length must be between 8 & 128 characters');
+    };
+
+
+
+    for(i = 0; i < length; i += allCriteria){
+      criteriaArr.forEach(type => {
+        const funcName = Object.keys(type)[0];
+          
+        generatedPassword += funcs[funcName];
+    
+      });
+    }
+
+  const final = generatedPassword.slice(0,length);
+  alert(final)
+  return final;
+}
+
+ 
+function writePassword() {
+
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 // Add event listener to generate button
-// generateBtn.addEventListener("click", function showPassword(){
-//   console.log(writePassword())
-// });
+form.addEventListener("click", writePassword())
 
-
-
-
-
-// console.log()
+form.addEventListener('submit',(e) =>{
+  e.preventDefault();
+});
